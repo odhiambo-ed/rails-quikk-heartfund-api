5a93b2eea6fa01fb018ad20ff2ff10c32b76d939
odhiambo-ed <odhiambo.edward111@gmail.com>
2025-01-16 19:54:09 +0300

    add donation controller methods

--- app/controllers/donations_controller.rb
+++ app/controllers/donations_controller.rb
@@ -1,2 +1,70 @@
+require "net/http"
+require "json"
+require "base64"
+require "time"
+require "openssl"
+require "uri"
+require "securerandom"
+
 class DonationsController < ApplicationController
+    QUIKK_URL = 'https://tryapi.quikk.dev/v1/mpesa/charge'
+  DATE_HEADER = 'date'
+
+  def create
+    donation = Donation.new(donation_params)
+
+    if donation.save
+      response = make_post_request(donation)
+      render json: { status: 'success', response: response.body }, status: :created
+    else
+      render json: { errors: donation.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def donation_params
+    params.require(:donation).permit(:amount, :reference, :customer_no)
+  end
+
+  def generate_hmac_signature
+    timestamp = Time.now.httpdate
+    to_encode = "#{DATE_HEADER}: #{timestamp}"
+
+    hmac = OpenSSL::HMAC.digest('SHA256', ENV['QUIKK_SECRET'], to_encode)
+    encoded = Base64.strict_encode64(hmac)
+    url_encoded = URI.encode_www_form_component(encoded)
+
+    auth_string = %(keyId="#{ENV['QUIKK_KEY']}",algorithm="hmac-sha256",headers="#{DATE_HEADER}",signature="#{url_encoded}")
+
+    [timestamp, auth_string]
+  end
+
+  def make_post_request(donation)
+    timestamp, auth_string = generate_hmac_signature
+
+    uri = URI.parse(QUIKK_URL)
+    client = Net::HTTP.new(uri.host, uri.port)
+    client.use_ssl = true
+    request = Net::HTTP::Post.new(uri)
+    request.content_type = 'application/vnd.api+json'
+    request[DATE_HEADER] = timestamp
+    request['Authorization'] = auth_string
+    request.body = {
+      data: {
+        id: SecureRandom.uuid,
+        type: 'charge',
+        attributes: {
+          amount: donation.amount,
+          posted_at: Time.now.utc.iso8601,
+          reference: donation.reference,
+          short_code: '174379',
+          customer_no: donation.customer_no,
+          customer_type: 'msisdn'
+        }
+      }
+    }.to_json
+
+    client.request(request)
+  end
 end
